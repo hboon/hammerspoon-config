4933a5a02ec8fbc38f38388aee99e05e3013a8b3
Hwee-Boon Yar <hboon@motionobj.com>
2016-04-10 16:17:01 +0800

    Automatically move .mobileprovision from ~/Downloads/

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require 'window'
+local myfile = require 'myfile'
 require 'mystring'
 require 'scratch'
 
@@ -7,3 +8,22 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
 	hs.reload()
 end)
 hs.alert.show("Hammerspoon config reloaded")
+
+local downloadsDir = os.getenv("HOME") .. "/Downloads/"
+downloadsWatcher = hs.pathwatcher.new(downloadsDir, function(changed)
+	local destDir = "~/Dropbox/hb-and-ml-common/Provisioning Profiles/"
+	destDir = myfile:escapeFileName(destDir)
+	for _,file in pairs(changed) do
+		local ext = ".mobileprovision"
+		--Must check against file (with space verbatim), and not against escapedSrc (with \\space)
+		if file:sub(-string.len(ext)) == ext and myfile:exists(file) then
+			local escapedSrc = myfile:escapeFileName(file)
+			os.execute("mv ${escapedSrc} ${destDir}" % {escapedSrc=escapedSrc, destDir=destDir})
+		end
+	end
+end)
+downloadsWatcher:start()
+
+hs.shutdownCallback = function()
+	downloadsWatcher:stop()
+end
