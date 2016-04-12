2dcb43d9072313bd2a908dadff2486f151aaaa94
Hwee-Boon Yar <hboon@motionobj.com>
2016-04-12 15:25:24 +0800

    Automatically move Airdropped images from ~/Downloads/ to ~/Desktop

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ downloadsDir = myfile:escapeFileName(downloadsDir)
 downloadsWatcher = hs.pathwatcher.new(downloadsDir, function(changed)
 	for _,file in pairs(changed) do
 		moveProvisioningProfiles(file)
+		moveAirDroppedImages(file)
 	end
 end)
 downloadsWatcher:start()
@@ -31,6 +32,24 @@ function moveProvisioningProfiles(filename)
 	if filename:sub(-string.len(ext)) == ext and myfile:exists(filename) then
 		local escapedSrc = myfile:escapeFileName(filename)
 		os.execute("mv ${escapedSrc} ${destDir}" % {escapedSrc=escapedSrc, destDir=destDir})
+		return true
+	end
+	return false
+end
+
+function moveAirDroppedImages(filename)
+	local destDir = os.getenv("HOME") .. "/Desktop/"
+	destDir = myfile:escapeFileName(destDir)
+	local ext = ".png"
+	local prefix = "img_"
+	local name = filename:match( "([^/]+)$" )
+	--Must check against filename (with space verbatim), and not against escapedSrc (with \\space)
+	if string.lower(filename:sub(-string.len(ext))) == ext and string.lower(name:sub(1, string.len(prefix))) == prefix and myfile:exists(filename) then
+		--TODO if file already exists, append "-1" to filename
+		local escapedSrc = myfile:escapeFileName(filename)
+		os.execute("mv ${escapedSrc} ${destDir}" % {escapedSrc=escapedSrc, destDir=destDir})
+		local msg = "Moved file ${name} from ${from} to ${to}" % {name=name, from="Downloads/", to="Desktop/"}
+		hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
 		return true
 	end
 	return false
